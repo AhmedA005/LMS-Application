d360f072331f633466c7b1373e017c21a0a6d202
momenmedhatsalem <momenmedhatsalem@gmail.com>
2024-12-16 22:59:28 +0200

    Added jwt components

---
+++ src/main/java/com/example/LMS/Authentication/UserInfoRepository.java
@@ -0,0 +1,10 @@
+package com.example.LMS.Authentication;
+import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.stereotype.Repository;
+import com.example.LMS.Authentication.UserInfo;
+import java.util.Optional; 
+  
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> { 
+    Optional<UserInfo> findByName(String username); 
+}
